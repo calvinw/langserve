b8b753915f557c7152f7b8053535690e579fc09e
calvinw <calvin.e.williamson@gmail.com>
2024-01-30 01:26:15 -0500

    Update main.js

--- vite-project/src/main.js
+++ vite-project/src/main.js
@@ -6,7 +6,7 @@ import { ChatOpenAI } from "@langchain/openai";
 
 const chatModel = new ChatOpenAI({
                         temperature:0,
-                        openAIApiKey: "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"
+                        openAIApiKey: ""
                       });
 const response = await chatModel.invoke("What is the conjoint analysis?");
 console.log(response.text)
